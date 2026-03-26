f519107ca63e0d6dc8f2f19ba58a9585f536a91b
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2026-03-26 12:48:22 +0100

    initial commit

---
+++ src/test/java/com/example/TaskManagerApp/TaskManagerApp/TaskManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TaskManagerApp.TaskManagerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
