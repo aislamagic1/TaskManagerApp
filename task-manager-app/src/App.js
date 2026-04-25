5205af0e24d48e22fd301f8fab38651cb6b645b9
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2026-04-25 14:15:29 +0200

    add login form and routes

--- task-manager-app/src/App.js
+++ task-manager-app/src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import LoginForm from './components/LoginForm';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <LoginForm/>
-    </div>
+    <Router>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+    </Router>
   );
 }
+
+function NotFound () {
+  const navigate = useNavigate();
+  return (
+	<div>
+	  <h1>404: Page not found</h1>
+	  <button onClick={() => navigate("/")}>Go to Login Page</button>
+	</div>
+  );
+};
 
 export default App;
--- task-manager-app/src/components/LoginForm.jsx
+++ task-manager-app/src/components/LoginForm.jsx
@@ -1,41 +1,64 @@
 import { useState } from "react";
-import { login } from "./api/authApi";
+import { login } from "../api/authApi";
+import "./LoginForm.css";
 
 function LoginForm(){
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
 
     async function handleLogin(){
         try {
+            setError("");
             await login(username, password);
         } catch (error) {
+            setError("Invalid username or password");
             console.log(error);
         }
     }
 
     return (
-        <div className="Login">
-            <h1>Login</h1>
-            <div className="LoginForm">
-                <div>
+        <div className="login-container">
+            <div className="login-card">
+                <h1>Login</h1>
+
+                {error && <p className="error">{error}</p>}
+
+                <div className="login-form">
                     <label>Username</label>
-                    <input type="text"
-                    name="username" 
-                    value={username} 
-                    onChange={(e) => setUsername(e.target.value)} />
+                    <input 
+                        type="text"
+                        name="username" 
+                        value={username} 
+                        onChange={(e) => setUsername(e.target.value)} 
+                        placeholder="Enter your useraname"
+                    />
                 </div>
-                <div>
+
+                <div className="login-form">
                     <label>Password</label>
                     <input type="password" 
-                    name="password" 
-                    value={password} 
-                    onChange={(e) => setPassword(e.target.value)} />
+                        name="password" 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)} 
+                        placeholder="Enter your password"
+                    />
                 </div>
-                <div>
-                    <button type="button" name="login" onClick={handleLogin}>Login</button>
-                </div>
+                
+                
+                <button className="login-button" type="button" name="login" onClick={handleLogin}>
+                    Login
+                </button>
+                
+
+                <p className="register-text">
+                    Don't have an account?{" "}
+                    <span>
+                        Register
+                    </span>
+                </p>
             </div>
         </div>
     )
