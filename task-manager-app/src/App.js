d1f5d1983a03a73ad7125141b89809daced0b95f
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2026-04-28 18:56:34 +0200

    add secure routes

--- task-manager-app/src/App.js
+++ task-manager-app/src/App.js
@@ -2,19 +2,42 @@ import './App.css';
 import HomeComponent from './components/HomeComponent';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate, Outlet, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
         <Routes>
+          <Route element={<PrivateRoutes />}>
+            <Route path="/home" element={<HomeComponent />} />
+          </Route>
           <Route path="/" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
-          <Route path="/home" element={<HomeComponent />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
     </Router>
   );
+}
+
+function PrivateRoutes() {
+    const token = localStorage.getItem("token");
+
+    return (
+        isValidToken(token) ? <Outlet/> : <Navigate to='/'/>
+    )
+}
+
+function isValidToken(token) {
+  if(!token) return false;
+
+  try {
+    const payload = JSON.parse(atob(token.split(".")[1]));
+
+    //convert into ms
+    return payload.exp * 1000 > Date.now();
+  } catch {
+    return false;
+  }
 }
 
 function NotFound () {
