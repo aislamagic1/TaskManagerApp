7ba892691b152415d1effa09f453b8d0ee4792ab
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2026-05-04 20:21:28 +0200

    task status now accepts string value

--- backend/src/main/java/com/example/TaskManagerApp/TaskManagerApp/enums/TaskStatus.java
+++ backend/src/main/java/com/example/TaskManagerApp/TaskManagerApp/enums/TaskStatus.java
@@ -1,7 +1,20 @@
 package com.example.TaskManagerApp.TaskManagerApp.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum TaskStatus {
-    TODO,
-    IN_PROGRESS,
-    DONE
+    TODO("TODO"),
+    IN_PROGRESS("IN_PROGRESS"),
+    DONE("DONE");
+
+    private final String label;
+
+    TaskStatus(String label) {
+        this.label = label;
+    }
+
+    @JsonValue
+    public String getLabel() {
+        return label;
+    }
 }
